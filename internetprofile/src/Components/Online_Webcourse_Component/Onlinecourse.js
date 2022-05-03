65c9dcdaa27e7983e25bce8f54541689dd7b307c
Your Name <you@example.com>
2022-05-03 16:30:33 +0530

    ScrappedLinkedIn details and Displyed

--- internetprofile/src/Components/Online_Webcourse_Component/Onlinecourse.js
+++ internetprofile/src/Components/Online_Webcourse_Component/Onlinecourse.js
@@ -1,11 +1,56 @@
+import { useMutation } from "@apollo/client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useUserContent } from "../../context/userContext";
+import { UPDATE_USER_LINKEDIN } from "../../graphql";
 import "./Onlinecourse.css";
+import Avatar from "react-avatar";
 
 const OnlineCoursesComponent = () => {
-  const { userData, currentUser } = useUserContent();
+  const { userData, currentUser, userId } = useUserContent();
+  const [linedInThere, setLinedInThere] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [linkedInData, setLinkedInData] = useState({});
+  useEffect(() => {
+    if (!currentUser) return;
+    if (currentUser?.linkedin_data) {
+      setLinedInThere(true);
+      const jsonParsedData = JSON.parse(currentUser?.linkedin_data);
+      setLinkedInData(jsonParsedData);
+    }
+  }, [currentUser]);
+
+  const [updateLinkedInData, { data, loading, error }] =
+    useMutation(UPDATE_USER_LINKEDIN);
+
+  const loadLinkedinDataAndUpdate = async () => {
+    setLoading(true);
+    try {
+      const linkedInProvider = (currentUser?.providers || []).find(
+        (provider) => provider.provider === "linkedIn"
+      );
+      const username = linkedInProvider?.value;
+      console.log(username);
+      const req = await axios.get(
+        `http://localhost:8000/user?username=${username}`
+      );
+      if (req.data) {
+        const jsonData = JSON.stringify(req.data);
+        updateLinkedInData({ variables: { userId, data: jsonData } });
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    if (data) {
+      if (data?.updateUsersPermissionsUser?.data?.id) {
+        window.location.reload();
+      }
+    }
+  }, [data]);
 
   return (
     <div className="OnlinecourseContainer d-flex flex-row">
@@ -58,11 +103,15 @@ const OnlineCoursesComponent = () => {
           <div className="profile_container shadow ">
             {/* image container */}
             <div className="profileImageContainer">
-              <img
-                src={userData?.avatar_url}
-                width={"100%"}
-                height={"100%"}
-                style={{ borderRadius: "inherit" }}
+              <Avatar
+                // value={currentUser?.deatils?.data?.attributes?.firstName}
+                name={
+                  currentUser?.deatils?.data?.attributes?.firstName +
+                  " " +
+                  currentUser?.deatils?.data?.attributes?.lastName
+                }
+                style={{ width: "100%", height: "100%", borderRadius: "10px" }}
+                size={"100%"}
               />
             </div>
 
@@ -99,21 +148,46 @@ const OnlineCoursesComponent = () => {
             </div>
           </div>
         </div>
-
         <div className="text-dark padding_left_and_top mt-5 w-100 ">
           <h6 className="h6">Online Presence</h6>
           <br></br>
-          <div className="bg-light w-25 mt-3  text-center">
-            <button className="btn btn-outline-danger ">Hacker rank</button>
-            <br></br>
-            {/* <button className="btn btn-primary btn-sm">Python</button><br></br> */}
-            <div className="card_container ">
-              <p className="coursetitle">Python</p>
-              <h3 className="mt-3 mb-3">76%</h3>
+          {!linedInThere && (
+            <button
+              style={{ marginTop: 20 }}
+              onClick={loadLinkedinDataAndUpdate}
+              className="btn btn-primary"
+            >
+              Load Linkedin
+            </button>
+          )}
+        </div>
+        {console.log(linkedInData, Object.keys(linkedInData))}
+        {Object.keys(linkedInData).length > 0 && (
+          <div className="text-left margin">
+            <button
+              onClick={loadLinkedinDataAndUpdate}
+              className="btn btn-outline-primary linkedinBtn"
+            >
+              Update Linkedin Data
+            </button>
+            <div className="OnlinedataContainer w-50 mt-3">
+              <h1 className="heading ">
+                Name : {linkedInData?.data?.name.trim()}
+              </h1>
+              <p className="p"> SKills : {linkedInData?.data?.skills}</p>
+              <p className="p">Bio : {linkedInData?.data?.bio}</p>
+              <div>
+                {linkedInData?.data?.experience.map((exp, index) => {
+                  return (
+                    <p className="p">
+                      Experience: {exp.role} @ {exp.name}
+                    </p>
+                  );
+                })}
+              </div>
             </div>
           </div>
-          {/* </div> */}
-        </div>
+        )}
       </div>
     </div>
   );
